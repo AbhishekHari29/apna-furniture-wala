80adfedaffe2c5f028f604ac03a8ed0b710aa3c4
Abhishek Hari <71129960+AbhishekHari29@users.noreply.github.com>
2022-02-03 00:46:55 +0530

    Form Storage and Mail

--- js/main.js
+++ js/main.js
@@ -1,101 +1,122 @@
 (function ($) {
-    "use strict";
+	"use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+	// Spinner
+	var spinner = function () {
+		setTimeout(function () {
+			if ($("#spinner").length > 0) {
+				$("#spinner").removeClass("show");
+			}
+		}, 1);
+	};
+	spinner();
 
+	// Initiate the wowjs
+	new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+	// Sticky Navbar
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$(".sticky-top").addClass("shadow-sm").css("top", "0px");
+		} else {
+			$(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
+		}
+	});
 
+	// Back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$(".back-to-top").fadeIn("slow");
+		} else {
+			$(".back-to-top").fadeOut("slow");
+		}
+	});
+	$(".back-to-top").click(function () {
+		$("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+		return false;
+	});
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+	// Facts counter
+	$('[data-toggle="counter-up"]').counterUp({
+		delay: 10,
+		time: 2000
+	});
 
+	// Header carousel
+	$(".header-carousel").owlCarousel({
+		autoplay: true,
+		smartSpeed: 1500,
+		items: 1,
+		dots: true,
+		loop: true,
+		nav: true,
+		navText: [
+			'<i class="bi bi-chevron-left"></i>',
+			'<i class="bi bi-chevron-right"></i>'
+		]
+	});
 
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
+	// Testimonials carousel
+	$(".testimonial-carousel").owlCarousel({
+		autoplay: true,
+		smartSpeed: 1000,
+		center: true,
+		dots: false,
+		loop: true,
+		nav: true,
+		navText: [
+			'<i class="bi bi-arrow-left"></i>',
+			'<i class="bi bi-arrow-right"></i>'
+		],
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			}
+		}
+	});
 
+	// Portfolio isotope and filter
+	var portfolioIsotope = $(".portfolio-container").isotope({
+		itemSelector: ".portfolio-item",
+		layoutMode: "fitRows"
+	});
+	$("#portfolio-flters li").on("click", function () {
+		$("#portfolio-flters li").removeClass("active");
+		$(this).addClass("active");
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
-
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
+		portfolioIsotope.isotope({ filter: $(this).data("filter") });
+	});
 })(jQuery);
 
+function LoadFile(event) {
+	var file = event.target.files[0];
+	var reader = new FileReader();
+	reader.onload = function (e) {
+		console.log(e.target.result);
+		var fileData = e.target.result.substr(e.target.result.indexOf(",") + 1);
+		var mimeTypeStart = e.target.result.indexOf("data:") + 5;
+		var mimeTypeEnd = e.target.result.indexOf(";");
+		var mimeType = e.target.result.substr(
+			mimeTypeStart,
+			mimeTypeEnd - mimeTypeStart
+		);
+		var fileName = file.name;
+		document.getElementById("fileData").value = fileData;
+		document.getElementById("mimeType").value = mimeType;
+		document.getElementById("fileName").value = fileName;
+	};
+	reader.readAsDataURL(file);
+}
+
+const scriptURL =
+	"https://script.google.com/macros/s/AKfycby122m8SpeJgEFdHxuw-ZdDPAB_EvS17qT8fThWt7rObLzKmdyoGWq-TRQk6sInezNC/exec";
+const quoteForm = document.forms["quote-form"];
+quoteForm.addEventListener("submit", async (e) => {
+	// e.preventDefault();
+	await fetch(scriptURL, { method: "POST", body: new FormData(quoteForm) })
+		.then((response) => console.log(response))
+		.catch((error) => console.log(error));
+});
