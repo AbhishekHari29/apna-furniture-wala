fdca60d2463889b1a6d39e88d63f424f445f8f8f
Abhishek Hari <71129960+AbhishekHari29@users.noreply.github.com>
2022-02-09 14:37:38 +0530

    Footer & Brands

--- js/main.js
+++ js/main.js
@@ -36,6 +36,38 @@
 		return false;
 	});
 
+	// Brands Slider
+	$(document).ready(function () {
+		if ($(".brands_slider").length) {
+			var brandsSlider = $(".brands_slider");
+
+			brandsSlider.owlCarousel({
+				loop: true,
+				autoplay: true,
+				autoplayTimeout: 2500,
+				nav: false,
+				dots: false,
+				autoWidth: true,
+				items: 8,
+				margin: 42
+			});
+
+			if ($(".brands_prev").length) {
+				var prev = $(".brands_prev");
+				prev.on("click", function () {
+					brandsSlider.trigger("prev.owl.carousel");
+				});
+			}
+
+			if ($(".brands_next").length) {
+				var next = $(".brands_next");
+				next.on("click", function () {
+					brandsSlider.trigger("next.owl.carousel");
+				});
+			}
+		}
+	});
+
 	// Facts counter
 	$('[data-toggle="counter-up"]').counterUp({
 		delay: 10,
